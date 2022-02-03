a1d0bb0090a197d34393491f2b5b3aed3c6b6d8a
yonaplacode <yonaplacode@gmail.com>
2022-02-03 15:41:27 -0300

    Change margin left TodoFilter

--- components/TodoFilter/TodoFilter.tsx
+++ components/TodoFilter/TodoFilter.tsx
@@ -10,7 +10,7 @@ const TodoFilter = () => {
   }
 
   return (
-    <div className='ml-8 mb-8 flex flex-row'>
+    <div className='mb-8 flex flex-row'>
       <input
         className="relative pl-8 w-56 h-12 rounded-full bg-white placeholder-gray-400 border-2 focus:outline-primary "
         placeholder='Filter your tasks'
